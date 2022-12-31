9625ae78079b2f00334f98fbc0cb47bf788b6f4c
Nir <nzadansk@univ.haifa.ac.il>
2022-12-31 22:46:29 +0000

    gui_player reactive

--- parser/gui_player.py
+++ parser/gui_player.py
@@ -5,10 +5,13 @@ from PIL import Image, ImageTk
 import csv
 import os
 
+CONTROL_FRAME_HEIGHT = 200
+
 class ImageViewer(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack(fill=tk.BOTH, expand=True)
+        self.configure(width=300, height=300)
 
         # Set up the GUI
         self.create_widgets()
@@ -23,63 +26,26 @@ class ImageViewer(tk.Frame):
         # Bind the resize event to the callback function
         self.bind("<Configure>", self.on_resize)
 
-        self.image = None  # Create the image attribute
-        self.image_width = 0
-        self.image_height = 0
-        self.original_width = 1
-        self.original_height = 1
         # Get the original size of the image
-        # if hasattr(self, "image") and self.image is not None:
-        #     self.original_width = self.image.width()
-        #     self.original_height = self.image.height()
-        # self.original_width = 100
-        # self.original_height = 100
+
 
     def on_resize(self, event):
         # Calculate the new size of the image based on the aspect ratio
-        aspect_ratio = self.original_width / self.original_height
-        # new_width = event.width
-        # new_height = int(event.width / aspect_ratio)
-
-        # new_width = self.image_label.winfo_width()
-        # new_height = self.image_label.winfo_height()
-
-        # print("event", event.width, event.height)
-        # print("label", new_width, new_height)
-
-        # # Resize the image to fit the widget
-        # # self.image = self.image.resize((new_width, new_height), Image.ANTIALIAS)
-        # # Convert the PhotoImage to an Image object
-        # # image = Image.frombytes(mode="RGB", size=(self.image.width(), self.image.height()),
-        # #                         data=self.image.tostring())
-
-        # # Resize the image
-        # # image = image.resize((new_width, new_height), Image.ANTIALIAS)
-
-        # # Convert the resized image back to a PhotoImage
-        # # self.image = ImageTk.PhotoImage(image)
-
-        # # Update the image size
-        # self.image_width = new_width // 2
-        # self.image_height = new_height
-
-        # print("width // 2", self.image_width, self.image_height)
-
-        # Update the size of the image
-        # self.configure(width=new_width, height=new_height)
-        # self.control_frame.configure(height=100)
+        # aspect_ratio = self.original_width / self.original_height
         self.update_image()
 
     def create_widgets(self):
         # Create the image label
         self.image_label = tk.Label(self)
         self.image_label.pack(fill=tk.BOTH, expand=True)
+        # set a size for the image label
+        # self.image_label.configure(height=300, width=300)
 
         # Create the control frame
         self.control_frame = tk.Frame(self)
-        self.control_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.control_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
         # set a size for the control frame
-        self.control_frame.configure(height=200)
+        self.control_frame.configure(height=CONTROL_FRAME_HEIGHT)
 
         # Create the "Open" button
         self.open_button = tk.Button(self.control_frame, text="Open", command=self.open_csv)
@@ -104,6 +70,21 @@ class ImageViewer(tk.Frame):
         # Create the progress bar
         self.progress_bar = ttk.Progressbar(self.control_frame, orient="horizontal", length=200)
         self.progress_bar.pack(side=tk.TOP, fill=tk.X, expand=True)
+
+        # Create the seeker
+        self.seeker = ttk.Scale(self.control_frame, from_=0, to=100, orient='horizontal', command=self.show_image_at_index)
+        self.seeker.pack(side='bottom', fill=tk.X, expand=True)
+
+        # Create the status label
+        self.status_label = tk.Label(self.control_frame, text="Status")
+        self.status_label.pack(side="bottom", fill=tk.X)
+
+        # Create an index label
+        self.index_label = tk.Label(self.control_frame, text="Index")
+        self.index_label.pack(side="bottom", fill=tk.X)
+
+    def show_image_at_index(self, index):
+        pass
 
     def open_csv(self):
         # Open a file dialog to select the CSV file
@@ -131,21 +112,44 @@ class ImageViewer(tk.Frame):
         # self.image_width = self.original_width
         # self.image_height = self.original_height
 
+        # self.image_label.configure(width=300, height=300)
+
         self.update_image()
         self.progress_bar.config(maximum=len(self.images))
+        self.seeker.config(to=len(self.images))
         self.paused = False
         self.speed = 1.0
 
     def update_image(self):
+        if not self.images:
+            return
+
         # Load the current image pair
         left_image_path, right_image_path = self.images[self.current_index]
         left_image = Image.open(left_image_path)
 
-        width, height = self.image_label.winfo_width() // 2 - 2, self.image_label.winfo_height() - 2
+        label_width = self.image_label.winfo_width()
+        label_height = self.image_label.winfo_height()
+        print("label_width", label_width, label_height)
+
+
+        width = label_width // 2 - 2
+        height = label_height - 2
         print("width // 2", width, height)
 
+        # print("control_frame", self.control_frame.winfo_width(), self.control_frame.winfo_height())
+        # if self.control_frame.winfo_height() < CONTROL_FRAME_HEIGHT:
+        #     self.control_frame.configure(height=CONTROL_FRAME_HEIGHT)
+
+
         # height = height - self.control_frame.winfo_height()
-        height = height - 50
+        # height = max(height - 50, 50)
+        height = max(height - 78 + self.control_frame.winfo_height(), 50)
+
+        print("control_frame", self.control_frame.winfo_width(), self.control_frame.winfo_height())
+        # if self.control_frame.winfo_height() < 78:
+        #     self.control_frame.configure(height=78)
+        #     height = height - 78
 
         # resize image
         left_image = left_image.resize((width, height), Image.ANTIALIAS)
@@ -208,6 +212,8 @@ class ImageViewer(tk.Frame):
     def update_progress(self):
         # Update the progress bar with the current image index
         self.progress_bar.config(value=self.current_index)
+        self.seeker.set(self.current_index)
+        self.index_label.config(text=f"Index: {self.current_index}")
 
 root = tk.Tk()
 app = ImageViewer(master=root)
